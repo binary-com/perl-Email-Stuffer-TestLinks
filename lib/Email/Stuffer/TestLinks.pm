aba9226e9fef16b88e9b01b649f3bbf69d9803d4
Bill Marriott <billmarriott@binary.com>
2018-05-11 02:09:20 +0000

    Fix line endings

--- lib/Email/Stuffer/TestLinks.pm
+++ lib/Email/Stuffer/TestLinks.pm
@@ -17,13 +17,13 @@ use Class::Method::Modifiers qw/ install_modifier /;
 
 =head1 NAME
 
-Email::Stuffer::TestLinks - validates links in HTML emails sent by 
+Email::Stuffer::TestLinks - validates links in HTML emails sent by
 Email::Stuffer>send_or_die()
 
 =head1 DESCRIPTION
 
-When this module is included in a test, it parses HTML links (<a href="xyz"...) 
-in every email sent through Email::Stuffer->send_or_die(). Each URI must get a 
+When this module is included in a test, it parses HTML links (<a href="xyz"...)
+in every email sent through Email::Stuffer->send_or_die(). Each URI must get a
 successful response code (200 range) and the returned pagetitle must not contain
 'error' or 'not found'.
 
