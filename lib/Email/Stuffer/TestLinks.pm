c2a42f8e12f5b949f913e99f4097d119ff84c31e
Bill Marriott <billmarriott@binary.com>
2019-09-26 04:53:09 +0000

    add circle-ci config

--- lib/Email/Stuffer/TestLinks.pm
+++ lib/Email/Stuffer/TestLinks.pm
@@ -27,8 +27,8 @@ Email::Stuffer>send_or_die()
 =head1 DESCRIPTION
 
 When this module is included in a test, it parses http links (<a href="xyz">...</a>)
-and image links (<img src="xyz">) in every email sent through Email::Stuffer->send_or_die(). 
-Each URI must be  get a successful response code (200 range). 
+and image links (<img src="xyz">) in every email sent through Email::Stuffer->send_or_die().
+Each URI must be  get a successful response code (200 range).
 Page title must not contain 'error' or 'not found' for text/html content.
 Image links must return an image content type.
 
@@ -37,7 +37,7 @@ Image links must return an image content type.
 install_modifier 'Email::Stuffer', after => send_or_die => sub {
 
     my $self = shift;
-    
+
     my %urls;
     $self->email->walk_parts(
         sub {
@@ -47,38 +47,38 @@ install_modifier 'Email::Stuffer', after => send_or_die => sub {
             push @{$urls{http}}, $dom->find('a')->map(attr => 'href')->compact->grep(sub {$_ !~ /^mailto:/})->uniq->to_array->@*;
             push @{$urls{image}}, $dom->find('img')->map(attr => 'src')->compact->uniq->to_array->@*;
     });
-     
+
     my @data = map { my $type = $_; map { [ $type, $_ ] } $urls{$type}->@* } keys %urls;
 
     my $loop = IO::Async::Loop->new();
     $loop->add( my $http = Net::Async::HTTP->new( max_connections_per_host=>3 ) );
-    
+
     ( fmap_void {
        my ($type, $url) = @$_;
-    
+
         my $uri = URI->new($url);
         unless ( $uri->scheme ) {
             fail "$type link $url is an invalid uri";
             return Future->done;
         }
-    
+
         $http->GET( URI->new($uri) )
             ->then( sub {
                 my $response = shift;
-                  
-                return Future->fail("Response code was ".$response->code) if ($response->code !~ /^2\d\d/);                    
-                  
+
+                return Future->fail("Response code was ".$response->code) if ($response->code !~ /^2\d\d/);
+
                 if ($response->content_type eq 'text/html') {
                    my $dom = Mojo::DOM->new($response->decoded_content);
                    if ( my $title = $dom->at('title') ) {
                         return Future->fail( "Page title contains text '$1'") if $title->text =~ /(error|not found)/i;
                    }
                 };
-               
+
                 if ( $type eq 'image' ) {
                    return Future->fail( "Unexpected content type: ".$response->content_type ) unless $response->content_type =~ /^image\//;
                 }
-               
+
                 return Future->done;
             } )
             ->transform(
@@ -93,7 +93,7 @@ install_modifier 'Email::Stuffer', after => send_or_die => sub {
             ->else(
                 sub { Future->done }
             )
-    } foreach => \@data, concurrent => 10 )->get;        
+    } foreach => \@data, concurrent => 10 )->get;
 
 };
 
